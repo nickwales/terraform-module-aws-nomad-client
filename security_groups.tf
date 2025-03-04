95d17a266f798dfd78976fd8316da18be5e685b6
Nick Wales <588472+nickwales@users.noreply.github.com>
2025-03-04 09:38:40 -0500

    Updates protocols for limited range

--- security_groups.tf
+++ security_groups.tf
@@ -4,10 +4,18 @@ resource "aws_security_group" "nomad_client" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "Platform ports"
+    description = "TCP Platform ports"
     from_port   = 0
     to_port     = 9000
-    protocol    = -1
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description = "UDP Platform ports"
+    from_port   = 0
+    to_port     = 9000
+    protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
